e22bac8814612c7a6138199ddcbd944c33bddab2
untill99 <gabriel.kasten@outlook.com>
2025-11-24 18:13:36 -0300

    feat(database): add PostgreSQL database integration
    - Introduced a new `.env` file for environment variable management.
    - Created `database.py` to handle SQLAlchemy engine and session creation.
    - Added `user.py` model for user data representation in the database.
    - Implemented `user_schema.py` for Pydantic models to validate user data.
    - Added Docker Compose configuration for PostgreSQL service.

--- sigo-api/controller/auth_controller.py
+++ sigo-api/controller/auth_controller.py
@@ -1,7 +1,17 @@
-from models.auth import Login
+from models.user import User
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from typing import Optional
 
 
 class AuthController:
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+        return pwd_context.verify(plain_password, hashed_password)
+
     @classmethod
-    async def login(cls, user: Login) -> Login:
-        return user
+    async def login(cls, db: Session, username: str, password: str) -> Optional[User]:
+        user = db.query(User).filter(User.username == username).first()
+        if user and user.verify_password(password):
+            return user
+        return None
