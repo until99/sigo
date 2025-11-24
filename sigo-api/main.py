75bd3c331b6c3660067b93e0fc141d45ecf6808f
untill99 <gabriel.kasten@outlook.com>
2025-11-24 18:13:36 -0300

    feat(database): add PostgreSQL database integration
    - Introduced a new `.env` file for environment variable management.
    - Created `database.py` to handle SQLAlchemy engine and session creation.
    - Added `user.py` model for user data representation in the database.
    - Implemented `user_schema.py` for Pydantic models to validate user data.
    - Added Docker Compose configuration for PostgreSQL service.

--- sigo-api/main.py
+++ sigo-api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from views.auth_view import router_auth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -8,4 +12,4 @@ app.router.include_router(router_auth, prefix="/v1", tags=["auth"])
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=int(os.getenv("SERVER_PORT", 8000)))
